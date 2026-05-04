16da81fda0ccd55e2e1859b122f6e6948bbf706f
Dadan Hamdani <dandikdas@gmail.com>
2026-05-04 11:47:43 +0900

    fix route home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-// di App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./routes/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route
           path="/profile"
           element={
@@ -22,4 +31,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;
